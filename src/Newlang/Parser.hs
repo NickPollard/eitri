55fddd16690a578fd8137ab9bc7965b2ccfb1220
Nick Pollard <nickedwardpollard@gmail.com>
2017-07-15 14:11:42 +0100

    Working on parser with Megaparsec

--- src/Newlang/Parser.hs
+++ src/Newlang/Parser.hs
@@ -1,28 +1,81 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Newlang.Parser (
 ) where
 
+import Control.Applicative (many, (<|>), liftA2)
+import Data.Bifunctor (second)
+import qualified Data.List as List
+import Data.List.NonEmpty
 import Data.Loc (L(..))
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.Text (Text, pack)
-import Text.Parsec
+import Text.Megaparsec.Combinator
+import Text.Megaparsec.Error
+import Text.Megaparsec.Prim
 
 import Newlang.AST
-import Newlang.Tokeniser (Token)
+import Newlang.Tokeniser (Tok)
+import qualified Newlang.Tokeniser as Tok
 import qualified Newlang.Tokeniser as T
 
-type Parser = Parsec [L Token] ()
+--type Parser = Parsec [L Token] ()
+newtype TokenStream = TokenStream [L Tok]
 
+instance Stream TokenStream where
+  type Token TokenStream = L Tok
+  uncons (TokenStream l) = second TokenStream <$> List.uncons l
+  updatePos = undefined -- TODO
+
+type MonadParser m = MonadParsec Dec TokenStream m
+
+type' :: MonadParser m => m Type
+type' = undefined -- TODO
+
+varDef :: MonadParser m => m SomeVarDef
+varDef = var <|> val
+  where var = fmap SomeVarDef $ Var <$> (reserved "var" *> name) <* reserved ":" <*> type'
+        val = fmap SomeVarDef $ Val <$> (reserved "val" *> name) <* reserved ":" <*> type'
 
 -- assignment: val name = expr
---assign      = Assign     <$> name <* string "=" <*> expr
+statement :: MonadParser m => m Statement
+statement =   Assign <$> name <* reserved "=" <*> expr
+          <|> Eval <$> expr
+          <|> Initialize <$> varDef <* reserved "=" <*> expr
+          <|> FunDef <$> (reserved "def" *> name) <*> parens (many argDef) <* reserved "=" <*> block
 
---expr :: Parser Expr
---expr = --applyFn | valueOf
-      --ValueOf <$> name
+parens :: MonadParser m => m a -> m a
+parens p = reserved "(" *> p <* reserved ")"
 
---name :: Parser Name
---name = parseToken match
-  --where match (Name t) = Just t
-        --match _        = Nothing
+argDef :: MonadParser m => m (VarName, Type)
+argDef = (name <* reserved ":") `tuple` type'
+
+tuple :: Applicative f => f a -> f b -> f (a,b)
+tuple = liftA2 (,)
+
+block :: MonadParser m => m [Statement]
+block = undefined -- TODO
+
+expr :: MonadParser m => m Expr
+expr =     (Apply <$> name <*> many expr)
+       <|> (ValueOf <$> name)
+
+name :: MonadParser m => m Name
+name = token test Nothing
+  where test (L _ (Tok.Name t)) = Right $ Name t
+        test other              = didntExpect other
+
+didntExpect :: (ErrorComponent e) => t -> Either (Set (ErrorItem t), Set (ErrorItem t), Set e) a
+didntExpect a = Left (Set.singleton (Tokens $ a :| []), Set.empty, Set.empty)
+
+reserved :: MonadParser m => Text -> m ()
+reserved word = token test Nothing
+  where test (L _ (Tok.ReservedWord w)) | word == w = Right ()
+        test other = didntExpect other
 
 --parseToken :: (L Token -> Maybe a) -> Parser a
 --parseToken = token show loc
--- src/Newlang/Tokeniser.hs
+++ src/Newlang/Tokeniser.hs
@@ -1,57 +1,74 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Newlang.Tokeniser (
-  Token,
+  Tok(..),
   tokenise
 ) where
 
 import Data.Functor.Identity (Identity)
 --import Data.Loc (L)
 import Data.Text (Text, pack)
-import Text.Megaparsec hiding (Token)
+import Text.Megaparsec
+import Text.Megaparsec.Prim (MonadParsec)
 import qualified Text.Megaparsec as MP
 --import Text.Megaparsec.Text
 
-data Token = Name Text
+data Tok = Name Text
            | ReservedWord Text
            | Symbol Text
-           deriving Show
+           deriving (Show, Eq, Ord)
 
 reservedWords = "def"
-         : "return"
-         : "if"
-         : "then"
-         : "else"
-         : "case"
-         : []
+              : "return"
+              : "if"
+              : "then"
+              : "else"
+              : "case"
+              : []
 
 symbol' = "("
-       : ")"
-       : "["
-       : "]"
-       : "}"
-       : "{"
-       : ":"
-       : "="
-       : []
+        : ")"
+        : "["
+        : "]"
+        : "}"
+        : "{"
+        : ":"
+        : "="
+        : []
 
+{-
 type Parser = ParsecT
               Dec       -- MegaParsec Default ErrorComponent
               Text      -- Parse from a `Text` stream
               Identity  -- runs in `Identity` Monad
+              -}
 
-txt :: String -> Parser Text
+type MonadParser m = MonadParsec Dec Text m
+
+txt :: MonadParser m => String -> m Text
 txt x = pack <$> string x
 
+name' :: MonadParser m => m Text
 name' = pack <$> ((:) <$> alphaNumChar <*> many alphaNumChar)
+
+symbols :: MonadParser m => m Text
 symbols = choice $ txt <$> symbol'
+
+reserved :: MonadParser m => m Text
 reserved = choice $ txt <$> reservedWords
 
-withSpaces = flip endBy space
+withSpaces :: MonadParser m => m a -> m a
+withSpaces p = p <* space
 
-tokeniser :: Parser [Token]
-tokeniser = withSpaces $ ReservedWord <$> reserved
+someToken :: MonadParser m => m Tok
+someToken = withSpaces $ ReservedWord <$> reserved
                      <|> Symbol <$> symbols
                      <|> Name <$> name'
 
-tokenise :: Text -> Either (ParseError (MP.Token Text) Dec)  [Token]
+tokeniser :: MonadParser m => m [Tok]
+tokeniser = many someToken
+
+tokenise :: Text -> Either (ParseError (Token Text) Dec)  [Tok]
 tokenise = parse (tokeniser <* eof) noFile
   where noFile = ""
